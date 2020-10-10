a58b713b75fae0646b97f53e35372945220138cf
livia01px2019 <livia.zhu@live.com>
2020-10-10 20:14:22 +0800

    vuex

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,8 +4,21 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    // access through this.$store.data.text etc
+    data() {
+      return { text: "", renderedTextArray: [] }
+    }
+  },
+  mutations: {
+    // mutate through this.$store.commit("updateText", payload);
+    updateText: (state, payload) => {
+      state.text = payload;
+    },
+    updateRenderedTextArray: (state, payload) => {
+      state.renderedTextArray = payload;
+    }
+  },
   actions: {},
   modules: {}
 });
