8e3d42a201583379079e41bb2337c073e5c1de27
crown523 <kxlider@rocketmail.com>
2020-10-10 20:43:18 +0800

    some functionality

--- client/src/components/Text.vue
+++ client/src/components/Text.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="outer col-4">
+    <div>
+      <button
+        dark
+        @click.stop="bold()"
+        icon
+        :color='grey'
+      >
+        Bold 
+      </button>
+      <button
+        dark
+        @click.stop="italicize()"
+        icon
+        :color='grey'
+      >
+        Italicize 
+      </button>
+      <button
+        dark
+        @click.stop="highlight()"
+        icon
+        :color='grey'
+      >
+        Highlight
+      </button>
+      <button
+        dark
+        @click.stop="render()"
+        icon
+        :color='grey'
+      >
+        Highlight
+      </button>
+    </div>
     <textarea
       id="message"
       rows="15"
@@ -21,7 +55,24 @@ export default {
       text: ""
     };
   },
-  methods: {}
+  methods: {
+    getSelection() {
+      console.log(window.getSelection);
+      return window.getSelection();
+    },
+    bold() {
+      let text = getSelection();
+
+    },
+    italicize() {
+      let text = getSelection();
+
+    },
+    highlight() {
+      let text = getSelection();
+
+    }
+  }
 };
 </script>
 
