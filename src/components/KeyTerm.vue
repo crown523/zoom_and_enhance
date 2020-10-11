71125a4c417b1a4389fcfd3b3206ffaadef5c0bd
livia01px2019 <livia.zhu@live.com>
2020-10-11 22:47:25 +0800

    styling

--- src/components/KeyTerm.vue
+++ src/components/KeyTerm.vue
@@ -1,31 +1,32 @@
 <template>
-    <div>
-        <button class="btn btn-info" @click="identifyKeyTerms">
-            Identify key terms
-        </button>
-        <p id="key-terms-list"></p>
-        <p> {{ keyTerms }}</p>
-    </div>
+  <div>
+    <button class="btn btn-info" @click="identifyKeyTerms">
+      Identify key terms
+    </button>
+    <p id="key-terms-list"></p>
+    <p>{{ keyTerms }}</p>
+  </div>
 </template>
 
 <script>
-import rake from 'rake-js';
+import rake from "rake-js";
 export default {
-    data() {
-        return {
-            keyTerms: ""
-        };
+  data() {
+    return {
+      keyTerms: ""
+    };
   },
-    methods: {
+  methods: {
     identifyKeyTerms() {
-        let extraction_result = rake(this.$store.state.text)
-        this.keyTerms = extraction_result;
-        
-        }
+      let extraction_result = rake(this.$store.state.text);
+      this.keyTerms = extraction_result;
     }
-}
-
+  }
+};
 </script>
 
 <style>
+button {
+  margin-top: 10px;
+}
 </style>
